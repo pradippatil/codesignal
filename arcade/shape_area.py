8b0b5c85637ecac12db12dd17e1230c27bcf76ff
Pradip Patil <pradippatil@users.noreply.github.com>
2018-12-20 00:10:46 +0000

    shape_area

---
+++ arcade/shape_area.py
@@ -0,0 +1,12 @@
+# -*- coding:utf-8 -*-
+# @Script: shape_area.py
+# @Author: Pradip Patil
+# @Contact: @pradip__patil
+# @Created: 2018-12-20 00:09:05
+# @Last Modified By: Pradip Patil
+# @Last Modified: 2018-12-20 00:09:31
+# @Description: https://app.codesignal.com/arcade/intro/level-2/yuGuHvcCaFCKk56rJ
+
+
+def shapeArea(n):
+    return n**2 + (n-1)**2
